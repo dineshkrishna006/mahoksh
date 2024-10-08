c7a3a540eafbcd76c6a0cad107ce3f86164199a1
dinesh-p0 <dineshkrishna0456@gmail.com>
2024-10-08 16:08:11 +0530

    infinitescroll

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -72,7 +72,7 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20 w-[100vw] overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20 w-[100vw] overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_10%,white_90%,transparent)]  lg:[mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
